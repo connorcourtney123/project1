3bd26d84d5fbb034e0ed72f035be09134f38029d
ConnorCourtney <connorcourtney123@gmail.com>
2021-03-21 16:52:54 -0400

    red counter working

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const COLOR_LIST = ['green', 'red', 'blue', 'yellow', 'white', 'purple']
 
 let code= []
 let currentGuess = []
+let pastGuesses = [] // this is an array of guess objects. guess objects holds GUESS and RED and WHITE values
+
 
 
 for(let i=0; i < 4; i++){
@@ -34,6 +36,32 @@ document.querySelector('#purplePrint').addEventListener('click', function(){
 
 
 function fingerClick(color) {
-    currentGuess.push(color)
-    console.log(currentGuess)
+    currentGuess.push(color);
+    if (currentGuess.length == 4){
+        var g = {guess: currentGuess, red: 0, white:0}
+        pastGuesses.push(g)
+        checkGuess(code, g)
+        currentGuess = []
+    }
+
+    
+    
+}
+
+function checkGuess(code, currentGuess){
+    
+    for(let i=0; i<4; i++){
+       if(code[i] == currentGuess.guess[i]){
+           
+           currentGuess.red++
+       } 
+    } 
+    console.log("pastGuesses after newest check")
+    console.log(pastGuesses)
+    if(currentGuess.red == 4){
+        
+        alert("YOU WIN")
+    } else{
+
+    }
 }
